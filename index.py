9cd106736ba4679918920b64ba5c2140fc8b4b1b
Siddhartha <75183632+siddhartha-devops@users.noreply.github.com>
2023-11-06 14:08:33 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello gyanu "
+    return "Hello gyanuS "
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("3000"), debug = True)
